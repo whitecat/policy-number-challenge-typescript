f7f1591db3a50c1aef0ec4c96dfe8308677e0217
leetcat <casey.thompson001@gmail.com>
2025-04-02 14:43:38 -0400

    Add parse entity method that will parse pipe strings
    [User story 1]

--- src/policy_ocr.ts
+++ src/policy_ocr.ts
@@ -1,3 +1,83 @@
 export class PolicyOcr {
+    private static ZERO =
+        " _ " +
+        "| |" +
+        "|_|";
 
+    private static ONE =
+        "   " +
+        "  |" +
+        "  |";
+
+    private static TWO =
+        " _ " +
+        " _|" +
+        "|_ ";
+
+    private static THREE =
+        " _ " +
+        " _|" +
+        " _|";
+
+    private static FOUR =
+        "   " +
+        "|_|" +
+        "  |";
+
+    private static FIVE =
+        " _ " +
+        "|_ " +
+        " _|";
+
+    private static SIX =
+        " _ " +
+        "|_ " +
+        "|_|";
+
+    private static SEVEN =
+        " _ " +
+        "  |" +
+        "  |";
+
+    private static EIGHT =
+        " _ " +
+        "|_|" +
+        "|_|";
+
+    private static NINE =
+        " _ " +
+        "|_|" +
+        " _|";
+
+    private static DIGIT_MAP: { [key: string]: string } = {
+        [PolicyOcr.ZERO]: '0',
+        [PolicyOcr.ONE]: '1',
+        [PolicyOcr.TWO]: '2',
+        [PolicyOcr.THREE]: '3',
+        [PolicyOcr.FOUR]: '4',
+        [PolicyOcr.FIVE]: '5',
+        [PolicyOcr.SIX]: '6',
+        [PolicyOcr.SEVEN]: '7',
+        [PolicyOcr.EIGHT]: '8',
+        [PolicyOcr.NINE]: '9'
+    };
+
+    static parseEntry(entryLine: string): string {
+        const entryLines = entryLine.split('\n')
+        if (entryLines.length != 4) {
+            throw new RangeError('Error parsing policy')
+        }
+        entryLines.every(line => line.length === 27) || (() => { throw new RangeError('Line length must be 27 characters'); })();
+
+        return [...Array(9).keys()]
+            .map(i => {
+                const index = i * 3;
+                const digitStr =
+                    entryLines[0].slice(index, index + 3) +
+                    entryLines[1].slice(index, index + 3) +
+                    entryLines[2].slice(index, index + 3);
+                return this.DIGIT_MAP[digitStr];
+            })
+            .join("");
+    }
 }
--- test/policy_ocr.test.ts
+++ test/policy_ocr.test.ts
@@ -10,4 +10,39 @@ describe('PolicyOcr', () => {
         const sampleContent = fs.readFileSync('./test/assets/sample.txt', 'utf-8');
         expect(sampleContent.split('\n').length).toBe(44);
     });
+
+    describe('User Story 1 - read in policy document', () => {
+        it('parse document throws error if entry line is not 4 lines', () => {
+            const entryLine = [
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|",
+            ].join('\n');
+
+            expect(() => PolicyOcr.parseEntry(entryLine)).toThrow(RangeError);
+        })
+        it('parse document throws error if entry line are not all 27 characters in length', () => {
+            const entryLine = [
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _",
+                "                           ",
+            ].join('\n');
+
+            expect(() => PolicyOcr.parseEntry(entryLine)).toThrow(RangeError);
+        })
+
+        it('parse document reads in a single line', () => {
+            const entryLine = [
+                "    _  _     _  _  _  _  _ ",
+                "  | _| _||_||_ |_   ||_||_|",
+                "  ||_  _|  | _||_|  ||_| _|",
+                "                           ",
+            ].join('\n');
+
+            PolicyOcr.parseEntry(entryLine)
+
+            expect(PolicyOcr.parseEntry(entryLine)).toBe("123456789");
+        })
+    })
 });
